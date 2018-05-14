d069949acd0386d9c7b39cfa006db9b59a6b75c4
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2018-05-14 02:19:14 -0400

    May-14-2018_2:19_AM

---
+++ ListViews/ListViews/ListViews/Model/Person.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListViews.Model
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+}
---
+++ ListViews/ListViews/ListViews/ViewModel/MainPageViewModel.cs
@@ -0,0 +1,24 @@
+﻿using ListViews.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace ListViews.ViewModel
+{
+    class MainPageViewModel
+    {
+        public ObservableCollection<Person> People { get; set; } = new ObservableCollection<Person>();
+
+        public MainPageViewModel()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Person person = new Person();
+                person.Name = "Jesse Liberty " + i.ToString();
+                person.Address = i.ToString() + " Main Street";
+                People.Add(person);
+            }
+        }
+    }
+}
